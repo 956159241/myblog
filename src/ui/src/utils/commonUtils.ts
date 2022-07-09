3d71e12d56e563f408b678177d52304672492680
zhonghua <956159241@qq.com>
2022-07-09 14:00:28 +0800

    feat: 优化clipboard无法在移动端使用bug

--- src/ui/src/utils/commonUtils.ts
+++ src/ui/src/utils/commonUtils.ts
@@ -3,7 +3,14 @@ import Clipboard from 'clipboard';
 export default class CommonUtils {
   static copy = (className: string, successInfo: string, errorInfo: string) => {
     const newClassName = className?.replaceAll(' ', '.')?.replaceAll(' ', '');
-    const clipboard = new Clipboard(`.${newClassName}`);
+    const clipboard = new Clipboard(`.${newClassName}`, {
+      text: function (trigger) {
+        console.log(trigger);
+        return (
+          trigger.getAttribute('data-clipboard-text') || '未知原因, 无法复制'
+        ); // 返回需要复制的内容
+      },
+    });
     clipboard.on('success', () => {
       popSuccessMessage(successInfo);
       // 释放内存
